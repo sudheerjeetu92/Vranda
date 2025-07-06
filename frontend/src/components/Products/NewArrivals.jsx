9e3cce918635238e34ce7bce9d85a594dad45ddb
Your Name <sudheerjeetu92@email.com>
2025-07-06 13:42:23 +0530

    5 attempt

--- frontend/src/components/Products/NewArrivals.jsx
+++ frontend/src/components/Products/NewArrivals.jsx
@@ -78,7 +78,7 @@ const NewArrivals = () => {
       updateScrollButtons();
       return () => container.removeEventListener("scroll", updateScrollButtons);
     }
-  },[newArrivals]);
+  }, [newArrivals]);
 
   return (
     <section className="py-16 px-4 lg:px-0">
@@ -131,12 +131,14 @@ const NewArrivals = () => {
             key={product._id}
             className="min-w-[50%] sm:min-w-50% lg:min-w-30% relative"
           >
-            <img
-              src={product.images[0]?.url}
-              alt={product.images[0]?.altText || product.name}
-              className="w-full h-[500px] object-cover rounded-lg"
-              draggable="false"
-            />
+            <Link to={`/product/${product._id}`}>
+              <img
+                src={product.images[0]?.url}
+                alt={product.images[0]?.altText || product.name}
+                className="w-full h-[500px] object-cover rounded-lg"
+                draggable="false"
+              />
+            </Link>
             <div className="absolute  bottom-0  left-0 right-0 bg-opacity-50 backdrop-blur-md text-white p-4 rounded-b-lg">
               <Link to={`/product/${product._id}`} className="block">
                 <h4 className="font-medium">{product.name}</h4>
