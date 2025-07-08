fdf62845d6deff8b361072fdff39b61702a3bd60
Your Name <sudheerjeetu92@email.com>
2025-07-08 21:12:21 +0530

    6 attempt

--- frontend/src/components/Products/ProductGrid.jsx
+++ frontend/src/components/Products/ProductGrid.jsx
@@ -12,7 +12,7 @@ const ProductGrid = ({ products, loading ,error }) => {
       {products.map((product, index) => (
         <Link key={index} to={`/product/${product._id}`} className="block">
           <div className="bg-white p-4 rounded-lg">
-            <div className="w-full h-96 mb-4">
+            <div className="aspect-[3/4] w-full mb-4">
               <img
                 src={product.images[0].url}
                 alt={product.images[0].alText || product.name}
