6e8cf65b4002d2270aa6af3ab5f9f9b493311bfa
Your Name <sudheerjeetu92@email.com>
2025-07-19 13:18:00 +0530

    Initial commit

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -112,7 +112,7 @@ const ProductDetails = ({ productId }) => {
               </div>
             </div>
             {/* Mobile Thumbnail */}
-            <div className="md:hidden flex overscroll-x-auto space-x-4 mb-4">
+            <div className="md:hidden flex overflow-x-auto space-x-4 mb-4 scrollbar-hide">
               {selectedProduct.images.map((image, index) => (
                 <img
                   key={index}
